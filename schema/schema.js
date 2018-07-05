7381895ae5d6dbf162564e7f30269a0a6c10d7ed
Adam Lorek <lorekadam@interia.pl>
2018-07-05 21:22:27 +0200

    working mutation, begining of modularizing schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,9 @@
-const { gql } = require('apollo-server');
+const { makeExecutableSchema, gql } = require('apollo-server');
 const User = require('../models/User');
 
+const Game = require('../models/Game');
+
+console.log(makeExecutableSchema);
 const typeDefs = gql`
   type Query {
     user(_id:String): BasicUserData
@@ -10,10 +13,40 @@ const typeDefs = gql`
     username: String,
     email: String
   }
+  type PlayerStats {
+    type: String,
+    score: Int,
+    shots: Int,
+    shotsOnTarget: Int,
+    possession: Int,
+    tackles: Int,
+    fouls: Int,
+    yellow: Int,
+    red: Int,
+    injuries: Int,
+    offsides: Int,
+    corners: Int,
+    shotsAccuracy: Int,
+    passAccuracy: Int
+  }
+  type Game {
+    _id: String,
+    homePlayer: String,
+    home: PlayerStats,
+    awayPlayer: String,
+    away: PlayerStats,
+    score: String
+  }
   type Mutation {
     addUser(username: String, email: String): BasicUserData
+    addGame(homePlayer: String, awayPlayer: String): Game
   }
 `;
+
+const Query = `
+type Query {
+  user(_id:String): BasicUserData
+}`
 
 // A map of functions which return data for the schema.
 const resolvers = {
@@ -21,8 +54,22 @@ const resolvers = {
     user: (root, args, context, info) => {
       return User.findOne(args);
     }
+  },
+  Mutation: {
+    addGame: (root, args, context, info) => {
+      let newGame = new Game({
+        homePlayer: args.homePlayer,
+        awayPlayer: args.awayPlayer
+      });
+      return newGame.save();
+    }
   }
 };
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 module.exports = {
   typeDefs,
--- schema/types/game_type.js
+++ schema/types/game_type.js
@@ -1,74 +1,75 @@
-const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+// const graphql = require('graphql');
+// const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
-const PlayerStatsType = new GraphQLObjectType({
-  name: 'GameStats',
-  fields: {
-    team: {
-      type: GraphQLString
-    },
-    score: {
-      type: GraphQLInt
-    },
-    shots: {
-      type: GraphQLInt
-    },
-    shotsOnTarget: {
-      type: GraphQLInt
-    },
-    possession: {
-      type: GraphQLInt
-    },
-    tackles: {
-      type: GraphQLInt
-    },
-    fouls: {
-      type: GraphQLInt
-    },
-    yellow: {
-      type: GraphQLInt
-    },
-    red: {
-      type: GraphQLInt
-    },
-    injuries: {
-      type: GraphQLInt
-    },
-    offsides: {
-      type: GraphQLInt
-    },
-    corners: {
-      type: GraphQLInt
-    },
-    shotsAccuracy: {
-      type: GraphQLInt
-    },
-    passAccuracy: {
-      type: GraphQLInt
-    }
-  }
-});
+// const PlayerStatsType = new GraphQLObjectType({
+//   name: 'GameStats',
+//   fields: {
+//     team: {
+//       type: GraphQLString
+//     },
+//     score: {
+//       type: GraphQLInt
+//     },
+//     shots: {
+//       type: GraphQLInt
+//     },
+//     shotsOnTarget: {
+//       type: GraphQLInt
+//     },
+//     possession: {
+//       type: GraphQLInt
+//     },
+//     tackles: {
+//       type: GraphQLInt
+//     },
+//     fouls: {
+//       type: GraphQLInt
+//     },
+//     yellow: {
+//       type: GraphQLInt
+//     },
+//     red: {
+//       type: GraphQLInt
+//     },
+//     injuries: {
+//       type: GraphQLInt
+//     },
+//     offsides: {
+//       type: GraphQLInt
+//     },
+//     corners: {
+//       type: GraphQLInt
+//     },
+//     shotsAccuracy: {
+//       type: GraphQLInt
+//     },
+//     passAccuracy: {
+//       type: GraphQLInt
+//     }
+//   }
+// });
 
-const GameType = new GraphQLObjectType({
-  name: 'Game',
-  fields: {
-    _id: { type: GraphQLString },
-    homePlayer: {
-      type: GraphQLString
-    },
-    home: {
-      type: PlayerStatsType
-    },
-    awayPlayer: {
-      type: GraphQLString
-    },
-    away: {
-      type: PlayerStatsType
-    },
-    score: {
-      type: GraphQLString
-    }
-  }
-});
+// const GameType = new GraphQLObjectType({
+//   name: 'Game',
+//   fields: {
+//     _id: { type: GraphQLString },
+//     homePlayer: {
+//       type: GraphQLString
+//     },
+//     home: {
+//       type: PlayerStatsType
+//     },
+//     awayPlayer: {
+//       type: GraphQLString
+//     },
+//     away: {
+//       type: PlayerStatsType
+//     },
+//     score: {
+//       type: GraphQLString
+//     }
+//   }
+// });
 
-module.exports = GameType;
+// module.exports = GameType;
+
