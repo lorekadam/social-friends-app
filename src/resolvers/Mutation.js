53a6676ff429be258273634d43c8f64e2a0aef1e
Adam Lorek <lorekadam@interia.pl>
2018-11-06 21:30:06 +0100

    working register on backend + begining on frontend

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createSport(parent, args, ctx, info) {
     const sport = await ctx.db.mutation.createSport(
@@ -9,6 +12,24 @@ const Mutation = {
       info
     );
     return sport;
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    return user;
   }
 };
 
