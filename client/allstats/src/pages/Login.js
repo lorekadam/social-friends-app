0d6b058244ccf60db56332cc5fca0dc3a5340f04
Adam Lorek <lorekadam@interia.pl>
2018-11-12 14:03:35 +0100

    login

--- client/allstats/src/pages/Login.js
+++ client/allstats/src/pages/Login.js
@@ -1,25 +1,89 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import { View } from 'react-native';
+import * as colors from '../styled/colors';
 import { Input } from '../styled/Input';
 import { Button } from '../styled/Button';
+import { Error } from '../styled/Error';
+import { Success } from '../styled/Success';
+import { Text } from '../styled/Text';
 
-// <Button
-//   title="Go to register page"
-//   onPress={() => this.props.navigation.navigate('Register')}
-// />
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
 
 export default class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      success: false
+    };
+  }
+  setValue = (name, val) => {
+    this.setState({
+      [name]: val
+    });
+  };
   render() {
+    const { email, password, success } = this.state;
     return (
-      <View>
-        <Button
-          title="Register page"
-          onPress={() => this.props.navigation.navigate('Register')}
-        />
-        <Input placeholder="Email..." />
-        <Input placeholder="Password..." />
-        <Button title="Login" onPress={() => console.log('test')} />
-      </View>
+      <Mutation mutation={SIGNIN_MUTATION} variables={this.state}>
+        {(signin, { error, loading }) => {
+          return (
+            <View>
+              <Button
+                title="Register page"
+                onPress={() => this.props.navigation.navigate('Register')}
+              />
+              <Input
+                value={email}
+                onChangeText={(val) => this.setValue('email', val)}
+                placeholder="Email"
+              />
+              <Input
+                value={password}
+                onChangeText={(val) => this.setValue('password', val)}
+                placeholder="Password"
+                secureTextEntry={true}
+              />
+              <Button
+                title="Login"
+                onPress={async () => {
+                  const res = await signin();
+                  if (res) {
+                    this.setState({
+                      email: '',
+                      password: '',
+                      success: true
+                    });
+                  }
+                }}
+              />
+              {error && (
+                <Error>
+                  <Text color={colors.white}>
+                    {error.message.replace('GraphQL error: ', '')}
+                  </Text>
+                </Error>
+              )}
+              {success && (
+                <Success>
+                  <Text color={colors.white}>Logged in!</Text>
+                </Success>
+              )}
+            </View>
+          );
+        }}
+      </Mutation>
     );
   }
 }
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -28,7 +28,19 @@ const Mutation = {
     );
 
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Errow(`No such user found for email ${email}`);
+    }
 
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error(`Invalid password!`);
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     return user;
   }
 };
