d3f4ee8f473baeebd9c48c5e6809b156f7d2cbea
Adam Lorek <lorek@bbdo.com.pl>
2018-12-19 16:05:32 +0100

    some things

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,10 +1,17 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  //   async sports(parent, args, ctx, info) {
-  //     const sports = await ctx.db.query.sports();
-  //     return sports;
-  //   }
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
